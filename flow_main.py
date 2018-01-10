21ea41ba543de78b66c1dd79b68e4ea1afe93934
Schraivogel <Schraivogel@users.noreply.github.com>
2018-01-10 14:51:28 +0100

    remove __main

--- flow_main.py
+++ flow_main.py
@@ -53,7 +53,7 @@ def f_init(f, w):
 
 
 def get_rho(f):
-	rho = np.sum(f, axis=2, dtype=float)
+	rho = np.sum(f, axis=2)
 	pMax = np.ones(rho.shape) + 1e-6
 	try:
 		np.testing.assert_array_less(rho, pMax)
@@ -94,7 +94,7 @@ def calc_equilibrium(rho, u, c, w):  # TODO: Use Einsum
 				part2 = (1 + 3 * uTmp.dot(cTmp) + 9/2 * uTmp.dot(cTmp) ** 2 - 3/2 * uTmp.dot(uTmp.T))
 
 				fEq[row, col, ch] = part1 * part2
-	print(rho)
+
 	return fEq
 
 
@@ -128,136 +128,133 @@ def init_u(epsilon, rows, cols):  # TODO: make dim variable. current is u_x with
 	return u
 		
 		
-if __name__ == "__main__":
-	####################################################################
-	######################## Initialization ############################
-	####################################################################
 
-	# lattice dimensions
-	nRows = 40
-	nCols = 40
-	nCh = 9
+####################################################################
+######################## Initialization ############################
+####################################################################
 
-	# number of timesteps
-	timesteps = 200
+# lattice dimensions
+nRows = 20
+nCols = 20
+nCh = 9
 
-	# velocity vector for matrix indices starting top left
-	c = np.array([[0, 0], [0, 1], [-1, 0], [0, -1], [1, 0], [-1, 1], [-1, -1], [1, -1], [1, 1]])
-	# lattice
-	f = np.zeros((nRows, nCols, nCh), dtype=float)
-	# weights for initial lattice distribution
-	w = np.array([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
+# number of timesteps
+timesteps = 200
 
-	# initial lattice distribution f
-	f = f_init(f, w)
+# velocity vector for matrix indices starting top left
+c = np.array([[0, 0], [1, 0], [0, -1], [-1, 0], [0, 1], [1, -1], [-1, -1], [-1, 1], [1, 1]])
+# lattice
+f = np.zeros((nRows, nCols, nCh), dtype=float)
+# weights for initial lattice distribution
+w = np.array([4/9, 1/9, 1/9, 1/9, 1/9, 1/36, 1/36, 1/36, 1/36])
 
-	# attenuation factor
-	omega = 0.1
-	assert 0 < omega < 1.7, 'Limits of attenuation factor exceeded'
+# initial lattice distribution f
+f = f_init(f, w)
 
-	# initialize shear wave decay factor
-	epsilon = 0.01
-	assert epsilon < 0.1, 'Limits of shear wave decay exceeded'
+# attenuation factor
+omega = 0.1
+assert 0 < omega < 1.7, 'Limits of attenuation factor exceeded'
 
-	####################################################################
-	##################### Start with initial rho #######################
-	####################################################################
+# initialize shear wave decay factor
+epsilon = 0.01
+assert epsilon < 0.1, 'Limits of shear wave decay exceeded'
 
-	# set density distribution offset
-	rhoOffset = 0.8
-	assert 0 < rhoOffset < 1.0, 'Limits of rhoT0 exceeded'
-	# initial density distribution at t = 0
-	rhoTZero = init_rho(epsilon, rhoOffset, nRows, nCols)
-	# set u to zero
-	uTZero = np.zeros((nRows, nCols, 2))
 
-	# Calculate lattice equilibrium according to given rho -> equal to f_eq
-	f = calc_equilibrium(rhoTZero, uTZero, c, w)
-	print(rhoTZero)
-	print(get_rho(f))
-	# sanity check if calculated rho from the lattice is equal to predefined rho at t = 0
-	assert np.isclose(rhoTZero, get_rho(f), rtol=1e-15, atol=1e-20, equal_nan=False).all(), 'Rho init failed'
+####################################################################
+##################### Start with initial rho #######################
+####################################################################
 
-	####################################################################
-	############### Start with initial average velocity u ##############
-	####################################################################
+# set density distribution offset
+rhoOffset = 0.8
+assert 0 < rhoOffset < 1.0, 'Limits of rhoT0 exceeded'
+# initial density distribution at t = 0
+rhoTZero = init_rho(epsilon, rhoOffset, nRows, nCols)
+# set u to zero
+uTZero = np.zeros((nRows, nCols, 2))
 
-	# initial density distribution at t = 0
-	uTZero = init_u(epsilon, nRows, nCols)
-	assert (abs(uTZero) < 0.1).all(), 'Limits of u exceeded'
-	# set rho to all ones
-	rhoTZero = np.ones((nRows, nCols))
+# Calculate lattice equilibrium according to given rho -> equal to f_eq
+f = calc_equilibrium(rhoTZero, uTZero, c, w)
 
-	# Calculate lattice equilibrium according to given rho -> equal to f_eq
-	f = calc_equilibrium(rhoTZero, uTZero, c, w)
+# sanity check if calculated rho from the lattice is equal to predefined rho at t = 0
+assert np.isclose(rhoTZero, get_rho(f), rtol=1e-15, atol=1e-20, equal_nan=False).all(), 'Rho init failed'
 
-	# sanity check if calculated u is equal to predefined u at t = 0
-	# compute j, rho-> == 1, u == uTZero
-	####################################################################
-	######################### Scattering ###############################
-	####################################################################
+####################################################################
+############### Start with initial average velocity u ##############
+####################################################################
 
-	rhoScatter = rhoTZero.copy()
-	uScatter = uTZero.copy()
-	rowPlot = 0
-	colPlot = 0
-	plotDiscret = 20
-	# Plotting
-	# Two subplots, the axes array is 1-d
-	fig = plt.figure()
-	ax = fig.add_subplot(111)    # The big subplot
-	ax1 = fig.add_subplot(311)
-	ax2 = fig.add_subplot(312)
-	ax3 = fig.add_subplot(313)
+# initial density distribution at t = 0
+uTZero = init_u(epsilon, nRows, nCols)
+assert (abs(uTZero) < 0.1).all(), 'Limits of u exceeded'
+# set rho to all ones
+rhoTZero = np.ones((nRows, nCols))
 
-	# Turn off axis lines and ticks of the big subplot
-	ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
+# Calculate lattice equilibrium according to given rho -> equal to f_eq
+f = calc_equilibrium(rhoTZero, uTZero, c, w)
 
-	# Set common labels
-	ax.set_xlabel('common xlabel')
-	ax.set_ylabel('common ylabel')
+# sanity check if calculated u is equal to predefined u at t = 0
+# compute j, rho-> == 1, u == uTZero
+####################################################################
+######################### Scattering ###############################
+####################################################################
 
-	ax1.set_title('Row of density rho')
-	ax2.set_title('Column of verage velocity u')
-	ax3.set_title('Values of verage velocity u at pi/4')
-	# storage of u vector points
-	uStore = np.zeros((1,0))
+rhoScatter = rhoTZero.copy()
+uScatter = uTZero.copy()
+rowPlot = 0
+colPlot = 0
+plotDiscret = 20
+# Plotting
+# Two subplots, the axes array is 1-d
+fig = plt.figure()
+ax = fig.add_subplot(111)    # The big subplot
+ax1 = fig.add_subplot(311)
+ax2 = fig.add_subplot(312)
+ax3 = fig.add_subplot(313)
 
-	# time loop
-	for t in range(timesteps):
+# Turn off axis lines and ticks of the big subplot
+ax.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
 
-		if t % plotDiscret == 0:
-			uStore = np.append(uStore, uScatter[nRows//4, colPlot, 0])
-			ax1.plot(rhoScatter[rowPlot, :], label='t = %s' % t)
-			print(rhoTZero)
-			ax2.plot(uScatter[:, colPlot, 0], label='t = %s' % t)
-			plt.draw()
-			plt.pause(1e-4)
-		# shift distribution f
-		f = shift_f(f)
-		# get partial current density j
-		j = calc_j(c, f)
-		# get current density rho
-		rhoScatter = get_rho(f)
-		# get average velocity
-		uScatter = calc_avg_vel(rhoScatter, j)
-		# get local equilibrium distributions
-		feQ = calc_equilibrium(rhoScatter, uScatter, c, w)
-		# update distribution
-		f += omega * (feQ - f)
+# Set common labels
+ax.set_xlabel('common xlabel')
+ax.set_ylabel('common ylabel')
 
-	print('Rho after %f timesteps: %s' % (timesteps, rhoScatter[0, :]))
+ax1.set_title('Row of density rho')
+ax2.set_title('Column of verage velocity u')
+ax3.set_title('Values of verage velocity u at pi/4')
+# storage of u vector points
+uStore = np.zeros((1,0))
 
-	ax3.plot(uStore)
+# time loop
+for t in range(timesteps):
 
-	# Legend
-	# Shrink current axis by 20%
-	box = ax.get_position()
-	ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-	# Put a legend to the right of the current axis
-	ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-	plt.show(ax) # figure blocks -> must be at the end of the code!
-	'''
-	rho = np.einsum('ijk->jk', f)
-	u = np.einsum('ai,ixy->axy',c,f)/rho
-	'''
+	if t % plotDiscret == 0:
+		uStore = np.append(uStore, uScatter[nRows//4, colPlot, 0])
+		ax1.plot(rhoScatter[rowPlot, :], label='t = %s' % t)
+		print(rhoTZero)
+		ax2.plot(uScatter[:, colPlot, 0], label='t = %s' % t)
+		plt.draw()
+		plt.pause(1e-4)
+	# shift distribution f
+	f = shift_f(f)
+	# get partial current density j
+	j = calc_j(c, f)
+	# get current density rho
+	rhoScatter = get_rho(f)
+	# get average velocity
+	uScatter = calc_avg_vel(rhoScatter, j)
+	# get local equilibrium distributions
+	feQ = calc_equilibrium(rhoScatter, uScatter, c, w)
+	# update distribution
+	f += omega * (feQ - f)
+
+print('Rho after %f timesteps: %s' % (timesteps, rhoScatter[0, :]))
+
+ax3.plot(uStore)
+
+# Legend
+# Shrink current axis by 20%
+box = ax.get_position()
+ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+# Put a legend to the right of the current axis
+ax1.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show(ax) # figure blocks -> must be at the end of the code!
+
