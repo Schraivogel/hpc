7d33694697834fa385d5a84fcdf672cc80f256e6
Schraivogel <Schraivogel@users.noreply.github.com>
2018-01-31 16:03:02 +0100

    mpi4py

--- test.py
+++ test.py
@@ -1,11 +1,19 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from threading import Timer
+import warnings
+from mpi4py import MPI
+
+import numpy as np
 import matplotlib
+import sys
 
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.cbook
 import warnings
-import time
+from mpi4py import MPI
 
 warnings.filterwarnings("ignore", category=matplotlib.cbook.mplDeprecation)
 
@@ -19,10 +27,10 @@ class Flowfield:
         self.c = np.array([[0, 0], [1, 0], [0, -1], [-1, 0], [0, 1], [1, -1], [-1, -1], [-1, 1], [1, 1]])
 
 
-def shift_f(grid, bounceMask, applyBounce):
+def shift_f(grid, bounceMask, applyBounce=False):
 
     if applyBounce:
-        saveGrid = np.ma.masked_where(bounceMask != True, grid)
+        saveGrid = np.ma.masked_where(bounceMask is True, grid)
 
     # center (0,0)
     # stays constant
@@ -59,21 +67,65 @@ def shift_f(grid, bounceMask, applyBounce):
     grid[:, :, ch] = np.roll(np.roll(grid[:, :, ch], shift=1, axis=0), shift=1, axis=1)
 
     if applyBounce:
-        # check top lid
-        if bounceMask[0, 0, 2]:
+        # treat corners for ch 5, 6, 7, 8 separately
+        if bounceMask[0, 0, 2] and bounceMask[-1, 0, 4]: # TODO: check every point along line
+            # top lid
             # channel 2->4, 5->7, 6->8
             grid[0, :, 4] = saveGrid[0, :, 2]
-            grid[0, :, 7] = saveGrid[0, :, 5]
-            grid[0, :, 8] = saveGrid[0, :, 6]
-        # check bottom lid
-        if bounceMask[-1, 0, 4]:
+            grid[0, 1:, 7] = saveGrid[0, 1:, 5]
+            grid[0, :-1, 8] = saveGrid[0, :-1, 6]
+            # bottom
             # channel 4->2, 7->5, 8->6
             grid[-1, :, 2] = saveGrid[-1, :, 4]
-            grid[-1, :, 5] = saveGrid[-1, :, 7]
-            grid[-1, :, 6] = saveGrid[-1, :, 8]
+            grid[-1, :-1, 5] = saveGrid[-1, :-1, 7]
+            grid[-1, 1:, 6] = saveGrid[-1, 1:, 8]
+
+        if bounceMask[0, 0, 3] and bounceMask[0, -1, 1]: # TODO: couette flow must still be possible
+            # left wall
+            # channel 3->1, 7->5, 6->8,
+            grid[:, 0, 1] = saveGrid[:, 0, 3]
+            grid[1:, 0, 5] = saveGrid[1:, 0, 7]
+            grid[:-1, 0, 8] = saveGrid[:-1, 0, 6]
+            # right wall
+            # channel 1->3, 8->6, 5->7
+            grid[:, -1, 3] = saveGrid[:, -1, 1]
+            grid[1:, -1, 6] = saveGrid[1:, -1, 8]
+            grid[:-1, -1, 7] = saveGrid[:-1, -1, 5]
+
+            # handle corners
+            grid[0, 0, 5] = saveGrid[0, 0, 5]
+            grid[-1, -1, 5] = saveGrid[-1, -1, 5]
+            grid[0, -1, 6] = saveGrid[0, -1, 6]
+            grid[-1, 0, 6] = saveGrid[-1, 0, 6]
+            grid[0, 0, 7] = saveGrid[0, 0, 7]
+            grid[-1, -1, 7] = saveGrid[-1, -1, 7]
+            grid[0, -1, 8] = saveGrid[0, -1, 8]
+            grid[-1, 0, 8] = saveGrid[-1, 0, 8]
 
     return grid
 
+def sliding_lid(grid, rho):
+    # according to slide 9 in HPC171129Ueb.pdf
+    # assume ch 2 and 4 are in equilibrium
+    fZero = grid[0, :, 0]
+    f1 = grid[0, :, 1]
+    f2 = grid[0, :, 2]
+    f3 = grid[0, :, 3]
+    f5 = grid[0, :, 5]
+    f6 = grid[0, :, 6]
+    rhoWall = fZero + f1 + f3 + 2 * (f2 + f5 + f6)
+
+    uLid = 0.1
+    # substract lid velocity
+    ch = 7
+    #grid[-1, :, ch] += (6 * w[ch] * rhoWall * c[ch, 0] * uLid).flatten()
+    grid[0, :, ch] -= (6 * w[ch] * rhoWall * uLid).flatten()
+    # add lid velocity
+    ch = 8
+    # grid[-1, :, ch] += (6 * w[ch] * rhoWall * c[ch, 0] * uLid).flatten()
+    grid[0, :, ch] += (6 * w[ch] * rhoWall * uLid).flatten()
+
+    return grid
 
 def f_init(f, w):
     nCh = len(w)
@@ -82,15 +134,16 @@ def f_init(f, w):
     return f
 
 
-def get_rho(f):
+def get_rho(f, checkMax=False):
     rho = np.sum(f, axis=2)
-    pMax = np.ones(rho.shape) + 1e-6
-    try:
-        np.testing.assert_array_less(rho, pMax)
-    except AssertionError:
-        with warnings.catch_warnings():
-            warnings.simplefilter("once")
-            warnings.warn("Rho bigger than one")
+    if checkMax:
+        pMax = np.ones(rho.shape) + 1e-6
+        try:
+            np.testing.assert_array_less(rho, pMax)
+        except AssertionError:
+            with warnings.catch_warnings():
+                warnings.simplefilter("once")
+                warnings.warn("Rho bigger than one")
     assert (rho.flatten() < 0).any() == False, 'Negative occupation / rho.'
     return rho
 
@@ -151,6 +204,104 @@ def init_u(epsilon, rows, cols):  # TODO: make dim variable. current is u_x with
     u[:, :, 0] = epsilon * np.sin(y).reshape(rows, 1)
     return u
 
+
+def save_mpiio(comm, fn, g_kl):
+    """
+    Write a global two-dimensional array to a single file in the npy format
+    using MPI I/O: https://docs.scipy.org/doc/numpy/neps/npy-format.html
+
+    Arrays written with this function can be read with numpy.load.
+
+    Parameters
+    ----------
+    comm
+        MPI communicator.
+    fn : str
+        File name.
+    g_kl : array_like
+        Portion of the array on this MPI processes. This needs to be a
+        two-dimensional array.
+    """
+    from numpy.lib.format import dtype_to_descr, magic
+    magic_str = magic(1, 0)
+
+    local_nx, local_ny = g_kl.shape
+    nx = np.empty_like(local_nx)
+    ny = np.empty_like(local_ny)
+
+    commx = comm.Sub((True, False))
+    commy = comm.Sub((False, True))
+    commx.Allreduce(np.asarray(local_nx), nx)
+    commy.Allreduce(np.asarray(local_ny), ny)
+
+    arr_dict_str = str({ 'descr': dtype_to_descr(g_kl.dtype),
+                         'fortran_order': False,
+                         'shape': (np.asscalar(nx), np.asscalar(ny)) })
+    while (len(arr_dict_str) + len(magic_str) + 2) % 16 != 15:
+        arr_dict_str += ' '
+    arr_dict_str += '\n'
+    header_len = len(arr_dict_str) + len(magic_str) + 2
+
+    offsetx = np.zeros_like(local_nx)
+    commx.Exscan(np.asarray(ny*local_nx), offsetx)
+    offsety = np.zeros_like(local_ny)
+    commy.Exscan(np.asarray(local_ny), offsety)
+
+    file = MPI.File.Open(comm, fn, MPI.MODE_CREATE | MPI.MODE_WRONLY)
+    if comm.Get_rank() == 0:
+        file.Write(magic_str)
+        file.Write(np.int16(len(arr_dict_str)))
+        file.Write(arr_dict_str.encode('latin-1'))
+    mpitype = MPI._typedict[g_kl.dtype.char]
+    filetype = mpitype.Create_vector(g_kl.shape[0], g_kl.shape[1], ny)
+    filetype.Commit()
+    file.Set_view(header_len + (offsety+offsetx)*mpitype.Get_size(),
+                  filetype=filetype)
+    file.Write_all(g_kl.copy())
+    filetype.Free()
+    file.Close()
+
+#  Note that these matrices need to be two-dimensional, i.e. you will need to write the individual cartesian
+#  components of a velocity vector to separate files. To write the velocities from your code, just execute:
+
+#save_mpiio(comm, 'ux.npy', velocities[0])
+#save_mpiio(comm, 'uy.npy', velocities[1])
+
+
+def mpi_communicate(comm, cartcomm, f):
+
+    leftSrc, leftDst = cartcomm.Shift(0, -1)
+    rightSrc, rightDst = cartcomm.Shift(0, 1)
+    upSrc, upDst = cartcomm.Shift(1, -1)
+    downSrc, downDst = cartcomm.Shift(1, 1)
+
+    # right
+    recvbuf = f[:, 0, :].copy() # TODO: init once
+    comm.Sendrecv(sendbuf=f[:, -1, :].copy(), dest=leftDst,
+                  recvbuf=recvbuf, source=leftSrc)
+    f[:, 0, :] = recvbuf
+
+    # left
+    recvbuf = f[:, -1, :].copy()  # TODO: init once
+    comm.Sendrecv(sendbuf=f[:, 0, :].copy(), dest=rightDst,
+                  recvbuf=recvbuf, source=rightDst)
+    f[:, -1, :] = recvbuf
+
+    # down
+    recvbuf = f[0, :, :].copy()  # TODO: init once
+    comm.Sendrecv(sendbuf=f[-1, :, :].copy(), dest=upDst,
+                  recvbuf=recvbuf, source=upSrc)
+    f[0, :, :] = recvbuf
+
+    # up
+    recvbuf = f[-1, :, :].copy()  # TODO: init once
+    comm.Sendrecv(sendbuf=f[0, :, :].copy(), dest=downDst,
+                  recvbuf=recvbuf, source=downSrc)
+    f[-1, :, :] = recvbuf
+
+    return f
+
+
 if __name__ == '__main__':
 
     ####################################################################
@@ -158,26 +309,39 @@ if __name__ == '__main__':
     ####################################################################
 
     # lattice dimensions
-    nRows = 5
-    nCols = 5
+    nRows = 200
+    nCols = 200
     nCh = 9
 
     # bounce back boundary
     bounceMask = np.zeros((nRows, nCols, nCh))
     # set borders to bounce back
+
+    bounceTopBottom = True
     # top lid
-    bounceMask[0, :, 2] = True
-    bounceMask[0, :, 5] = True
-    bounceMask[0, :, 6] = True
-    # bottom lid
-    bounceMask[-1, :, 4] = True
-    bounceMask[-1, :, 7] = True
-    bounceMask[-1, :, 8] = True
+    bounceMask[0, :, 2] = bounceTopBottom
+    bounceMask[0, :, 5] = bounceTopBottom
+    bounceMask[0, :, 6] = bounceTopBottom
+    # bottom
+    bounceMask[-1, :, 4] = bounceTopBottom
+    bounceMask[-1, :, 7] = bounceTopBottom
+    bounceMask[-1, :, 8] = bounceTopBottom
+
+    bounceLeftRight = True
+    # left wall
+    bounceMask[:, 0, 3] = bounceLeftRight
+    bounceMask[:, 0, 6] = bounceLeftRight
+    bounceMask[:, 0, 7] = bounceLeftRight
+    # right wall
+    bounceMask[:, -1, 1] = bounceLeftRight
+    bounceMask[:, -1, 5] = bounceLeftRight
+    bounceMask[:, -1, 8] = bounceLeftRight
 
     applyBounce = True
+    applySlidingLid = True
 
     # number of timesteps
-    timesteps = 100
+    timesteps = 1000
 
     # lattice
     f = np.zeros((nRows, nCols, nCh), dtype=float)
@@ -190,52 +354,28 @@ if __name__ == '__main__':
     f = f_init(f, w)
 
     # attenuation factor
-    omega = 0.1
-    assert 0 < omega < 1.7, 'Limits of attenuation factor exceeded'
+    omega = 0.7 # TODO: compute reynolds number >! 1000 for turbulent flow
+    assert 0 < omega <= 1.7, 'Limits of attenuation factor exceeded'
 
     # initialize shear wave decay factor
     epsilon = 0.01
-    assert epsilon < 0.1, 'Limits of shear wave decay exceeded'
+    assert epsilon <= 0.1, 'Limits of shear wave decay exceeded'
 
     ####################################################################
-    ##################### Start with initial rho #######################
+    ######################### comm init ###############################
     ####################################################################
+    comm = MPI.COMM_WORLD
 
-    startWithInitRho = False
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+    print('rank = {}/{}'.format(rank, size))
 
-    if startWithInitRho:
-        # set density distribution offset
-        rhoOffset = 0.8
-        assert 0 < rhoOffset < 1.0, 'Limits of rhoT0 exceeded'
-        # initial density distribution at t = 0
-        rhoTZero = init_rho(epsilon, rhoOffset, nRows, nCols)
-        # set u to zero
-        uTZero = np.zeros((nRows, nCols, 2))
+    dimSquare = 2
+    dims = (dimSquare, dimSquare)
+    # periodic communication disabled
+    cartcomm = comm.Create_cart(dims, periods=False, reorder=False)
 
-        # Calculate lattice equilibrium according to given rho -> equal to f_eq
-        f = calc_equilibrium(rhoTZero, uTZero, c, w)
-
-        # sanity check if calculated rho from the lattice is equal to predefined rho at t = 0
-        assert np.isclose(rhoTZero, get_rho(f), rtol=1e-15, atol=1e-20, equal_nan=False).all(), 'Rho init failed'
-
-    ####################################################################
-    ############### Start with initial average velocity u ##############
-    ####################################################################
-
-    startWithInitVelocity = False
-
-    if startWithInitVelocity:
-        # initial density distribution at t = 0
-        uTZero = init_u(epsilon, nRows, nCols)
-        assert (abs(uTZero) < 0.1).all(), 'Limits of u exceeded'
-        # set rho to all ones
-        rhoTZero = np.ones((nRows, nCols))
-
-        # Calculate lattice equilibrium according to given rho -> equal to f_eq
-        f = calc_equilibrium(rhoTZero, uTZero, c, w)
-
-        # sanity check if calculated u is equal to predefined u at t = 0
-        # compute j, rho-> == 1, u == uTZero
+    localSplit = nRows // dimSquare
 
     ####################################################################
     ##################### Start with sliding lid #######################
@@ -244,144 +384,44 @@ if __name__ == '__main__':
     startWithSlidingLid = True
 
     if startWithSlidingLid:
+        localGhost = localSplit + 2
         # initialize velocity only for top lid in x direction
-        uTZero = np.zeros((nRows, nCols, 2))
-        uTZero[0, :, 0] = 0.01
+        uTZero = np.zeros((localGhost, localGhost, 2))
         assert (abs(uTZero) < 0.1).all(), 'Limits of u exceeded'
         # set rho
-        rhoTZero = np.ones((nRows, nCols))
+        rhoTZero = np.ones((localGhost, localGhost))
+
         # Calculate lattice equilibrium according to given rho -> equal to f_eq
         f = calc_equilibrium(rhoTZero, uTZero, c, w)
 
-    ####################################################################
-    ######################### Scattering ###############################
-    ####################################################################
-
     rhoScatter = rhoTZero.copy()
     uScatter = uTZero.copy()
-    rowPlot = 0
-    colPlot = 0
-
-    # Plotting
-    showPlot = True
-    plotDiscret = 1
-    # Two subplots, the axes array is 1-d
-    fig1 = plt.figure(figsize=(10, 9))
-    ax1 = fig1.add_subplot(111)  # The big subplot
-    ax11 = fig1.add_subplot(211)
-    ax12 = fig1.add_subplot(212)
-
-    # Turn off axis lines and ticks of the big subplot
-    ax1.tick_params(labelcolor='w', top='off', bottom='off', left='off', right='off')
-
-    # Set labels
-    # ax.set_xlabel('common xlabel')
-    # ax.set_ylabel('common ylabel')
-    ax11.set_title('Column of average velocity u')
-    ax12.set_title('Average velocity u at pi/4')
-
-    fig2 = plt.figure(2)
-    plt.title('Average velocity u')
-
-    # storage of u vector points
-    uStore = []
-    # for streamplot
-    x = np.arange(nRows)
-    y = np.arange(nCols)
-    X, Y = np.meshgrid(x, y)
 
     # time loop
     for i in range(timesteps):
 
-        if showPlot:
-            if i % plotDiscret == 0:
-                if startWithInitVelocity:
-                    ax11.plot(uScatter[:, colPlot, 0], label='t = %s' % i)
-                else:
-                    plt.close(fig1)
-                # plot velocity streamfield
-                fig2.clf()
-                plt.quiver(X, Y, uScatter[:,:,0].T, uScatter[:,:,1].T, color='b')
-                plt.pause(1e-6)
-                time.sleep(0.25)
-
-        uStore = np.append(uStore, uScatter[nRows // 4, colPlot, 0])
-
+        if (i + 1) % 50 == 0:
+            print("\rTime {}/{}".format(i + 1, timesteps), end="")
+            sys.stdout.flush()
         # shift distribution f
         f = shift_f(f, bounceMask, applyBounce)
+        # slide lid
+        if applySlidingLid:
+            f = sliding_lid(f, rhoScatter)
         # get partial current density j
         j = calc_j(c, f)
         # get current density rho
         rhoScatter = get_rho(f)
         # get average velocity
-        print('U before', uScatter[:, :, 0])
-        print('Rho after', rhoScatter)
         uScatter = calc_avg_vel(rhoScatter, j)
-        print('After', uScatter[:,:,0])
         # get local equilibrium distributions
         feQ = calc_equilibrium(rhoScatter, uScatter, c, w)
         # update distribution
         f += omega * (feQ - f)
+        # mpi communication
+        f = mpi_communicate(comm, cartcomm, f)
 
-    plt.show()
+    save_mpiio(cartcomm, 'ux.npy', uScatter[1:-1, 1:-1, 0])
+    save_mpiio(cartcomm, 'uy.npy', uScatter[1:-1, 1:-1, 1])
 
-    calcViscosity = False
-
-    if calcViscosity:
-        print('Rho after %d timesteps: %s \n' % (timesteps, rhoScatter[0, :]))
-        t = np.arange(timesteps)
-        lnU = np.log(uStore)
-
-        # get slope of exp. fct via log formula
-        Ly = nCols
-        k = 2 * np.pi / Ly
-        point = 20
-        # compute viscosity(nu) the naive way
-        nuNoise = (-np.log(uStore[point]) + np.log(uStore[0])) / (k ** 2 * point)
-        nuNoiseAll = (-np.log(uStore[t[1::]]) + np.log(uStore[0])) / (k ** 2 * t[1::])
-        print('Naive computation of nu = %.3f, mean(nu) = %.3f \n' % (nuNoise, np.mean(nuNoiseAll)))
-        eFctNaive = uStore[0] * np.exp(-nuNoise * k ** 2 * t)
-
-        # analytical solution of exponential fit
-        t = np.arange(timesteps).reshape((-1, 1))
-        phi = np.concatenate((t, np.ones((len(t), 1))), axis=1)
-        coeffLn = np.linalg.inv(phi.T.dot(phi)).dot(phi.T).dot(lnU)
-
-        # line equation
-        print('Coefficients for linear log(u) fit = %s \n' % coeffLn)
-        line = coeffLn[0] * t + coeffLn[1]
-
-        # get coefficients for exponential equation
-        A = np.exp(coeffLn[1])
-        lam = coeffLn[0]
-        print('A = %.3f, lambda = %.3f for exponential fit \n' % (A, lam))
-        eFctFit = A * np.exp(lam * t)
-
-        # compute viscosity(nu) the exact way
-        nuExact = (-np.log(eFctFit[point]) + np.log(eFctFit[0])) / (k ** 2 * point)
-        print('Exact computation of nu = %.3f \n' % nuExact)
-
-        if showPlot:  # figure blocks -> must be at the end of the code!
-            # plot log of u values
-            ax12.plot(lnU, label='log(u)')
-            # plot linear fit
-            ax12.plot(line, 'r--', label='fit of log(u)')
-            # plot exp fit
-            # ax2.plot(eFct, 'g--', label='fit of $e^\lambda*t$')
-            # Legend
-            # Shrink current axis by 20%
-            box = ax1.get_position()
-            ax1.set_position([box.x0, box.y0, box.width * 0.8, box.height])
-            # Put a legend to the right of the current axis
-            ax11.legend(loc='center left', bbox_to_anchor=(1, 0.5))
-            ax12.legend(loc='best')
-            # plot u & exponential fit
-            fig2, ax2 = plt.subplots(1, 1)
-            ax2.set_title('Average velocity u')
-            ax2.plot(t, uStore, label='u')
-            ax2.plot(t, eFctFit, 'g--', label='$y = A*e^{\lambda*x}$')
-            ax2.plot(t, eFctNaive, 'k--', label='$u_0*e^{-\\nu*k^2*t}$', linewidth=0.6)
-            ax2.legend(loc='best')
-
-            plt.show()  # important to be at the end
 
