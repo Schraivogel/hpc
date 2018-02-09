4556c101eeba52187ced82e36fddd1d587f6b92e
ss1348 <stephan.schraivogel@venus.uni-freiburg.de>
2018-02-09 21:05:48 +0100

    minor

--- run_flow.sh
+++ run_flow.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -x
-#MSUB -l nodes=1:ppn=16,pmem=16000mb
+#MSUB -l nodes=1:ppn=4,pmem=16000mb
 #MSUB -l walltime=03:00:00
 #MSUB -N HPC_WITH_PYTHON
-
 
 module load devel/python/3.6.0
 module load mpi/openmpi/2.1-gnu-5.2
 
-
-
 echo "Running on ${MOAB_PROCCOUNT} cores."
-time mpiexec -n ${MOAB_PROCCOUNT} python3 flow_main_mpi.py
+time mpirun -n ${MOAB_PROCCOUNT} $HOME/venvs/rl/bin/python3.exe $HOME/git/hpc/flow_main_mpi.py
--- startScriptJob.sh
+++ startScriptJob.sh
@@ -1,2 +1,2 @@
-msub -m ae -M stephanschraivogel@gmail.com -l nodes=1:ppn=4 run_flow.sh
-
+msub run_flow.sh
+# -m ae -M stephanschraivogel@gmail.com
